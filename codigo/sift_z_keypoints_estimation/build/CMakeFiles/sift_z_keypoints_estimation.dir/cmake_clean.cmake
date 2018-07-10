50c0d8797af62a7430495734ca15cc7ff8922776
javierpp1381 <40938854+javierpp1381@users.noreply.github.com>
2018-07-10 20:49:45 +0200

    Add files via upload

---
+++ codigo/sift_z_keypoints_estimation/build/CMakeFiles/sift_z_keypoints_estimation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sift_z_keypoints_estimation.dir/sift_z_keypoints_estimation.cpp.o"
+  "sift_z_keypoints_estimation.pdb"
+  "sift_z_keypoints_estimation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sift_z_keypoints_estimation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
