9f261bcb56bac71da1e901506de06f93e6a9d12f
javierpp1381 <40938854+javierpp1381@users.noreply.github.com>
2018-08-08 18:28:20 +0200

    Add files via upload

--- codigo/visualization_sift/visualization.cpp
+++ codigo/visualization_sift/visualization.cpp
@@ -1,12 +1,31 @@
+
+//----------------------------------------------------------------------
+//-----------------------------------LIBRARIES--------------------------
+//----------------------------------------------------------------------
+
+
 #include <pcl/visualization/cloud_viewer.h>
 #include <iostream>
+#include <string>
 #include <pcl/io/io.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/console/parse.h>
     
 int user_data;
-    
 pcl::PointCloud<pcl::PointXYZRGBA>::Ptr sift_cloud (new pcl::PointCloud<pcl::PointXYZRGBA>);
+
+//----------------------------------------------------------------------
+//------------------------------METHODS---------------------------------
+//----------------------------------------------------------------------
+   
+
+void 
+printUsage (const char* progName)
+{
+  std::cout << "\n\nUsage: "<<progName<<" <cloud.pcd> <SIFTpoints.pcd\n\n";
+}
+
+
 void 
 viewerOneOff (pcl::visualization::PCLVisualizer& viewer)
 {
@@ -16,8 +35,9 @@ viewerOneOff (pcl::visualization::PCLVisualizer& viewer)
     o.y = 0;
     o.z = 0;
     viewer.addSphere (o, 0.01, "central sphere", 0);
+    
     for(size_t i=0;i<sift_cloud->points.size();i++){
-    	viewer.addSphere(sift_cloud->points[i],0.02f,50,255,50,"sphere"+i);	
+    	viewer.addSphere(sift_cloud->points[i],0.02f,50,255,50,std::to_string(i));	
     }
 }
     
@@ -32,31 +52,19 @@ viewerPsycho (pcl::visualization::PCLVisualizer& viewer)
     
     user_data++;
 }
+
+//----------------------------------------------------------------------
+//------------------------------MAIN------------------------------------
+//----------------------------------------------------------------------
     
 int 
 main (int argc, char** argv)
 {   
-
-   /* 
-    pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGBA>);
-     std::vector<int> pcd_filename_indices = pcl::console::parse_file_extension_argument (argc, argv, "pcd"); 
-     std::string filename;
-     
-     if (!pcd_filename_indices.empty ())
-  {
-     filename = argv[pcd_filename_indices[0]];
-    if (pcl::io::loadPCDFile (filename, *cloud) == -1)
+    if(argc == 1 || argc ==2 || (pcl::console::find_argument (argc,argv,"-h")) >= 0 )
     {
-      cerr << "Was not able to open file \""<<filename<<"\".\n";
-      return -1;
-    }
-  }
-    else
-  {
-    cout << "\nNo *.pcd file given => closing.\n\n";
-    return -1;
-  }
-   */
+	printUsage (argv[0]);
+	return 0;
+    } 
     pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGBA>);
    
     pcl::io::loadPCDFile(argv[1],*cloud);
@@ -70,21 +78,16 @@ main (int argc, char** argv)
     viewer.showCloud(cloud,"cloud");
     viewer.showCloud(sift_cloud,"sift_cloud");
    
-     
-    //use the following functions to get access to the underlying more advanced/powerful
-    //PCLVisualizer
-    
     //This will only get called once
     viewer.runOnVisualizationThreadOnce (viewerOneOff);
     
     //This will get called once per visualization iteration
     //viewer.runOnVisualizationThread (viewerPsycho);
+    
+    
     while (!viewer.wasStopped ())
     {
-    //you can also do cool processing here
-    //FIXME: Note that this is running in a separate thread from viewerPsycho
-    //and you should guard against race conditions yourself...
-    user_data++;
+    
     }
     return 0;
 }
